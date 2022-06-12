b15b8b19066a7cb4e590ed2376c1a2ffc1dd48d1
majerdaniel93@gmail.com <majerdaniel93@gmail.com>
2022-06-12 10:31:26 +0200

    init

---
+++ vim_backup/4-fetch_conda_libraries.sh
@@ -0,0 +1,8 @@
+conda install -n py39 -c anaconda jinja2 -y
+conda install -n py39 -c anaconda markdown -y
+conda install -n py39 -c anaconda colorama -y
+conda install -n py39 -c anaconda pandas -y
+conda install -n py39 -c anaconda pytest -y
+conda install -n py39 -c anaconda flask -y
+conda install -n py39 -c anaconda pytest -y
+conda install -n py39 -c conda-forge biopython -y
