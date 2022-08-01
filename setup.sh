b99f4cf2254a9ca7e531f9af249db0f2965beb9b
majerdaniel93@gmail.com <majerdaniel93@gmail.com>
2022-08-01 14:17:28 +0200

    bit of disdat

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update -y
 sudo apt-get install -y neovim
+sudo apt install -y xclip
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -12,3 +13,5 @@ echo "alias vim='nvim'" >> ~/.bashrc
 
 mkdir -p ~/.config/nvim/
 cp ./init.lua ~/.config/nvim/
+
+
