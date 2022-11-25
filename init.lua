168c5f0b80f56608c432f670e4938c7a4b1fba8e
daniel majer <danielmajer@pop-os.localdomain>
2022-11-25 10:50:32 +0100

    neovim:init.lua reduced plugins, they were not necessary and haven't been using them, like telescope...

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ local o   = vim.o
 local opt = vim.opt
 local A   = vim.api
 
+-- COLORS
+vim.cmd("colorscheme desert")
+-- vim.cmd[[hi Pmenu ctermfg=NONE ctermbg=236 cterm=NONE guifg=NONE guibg=#64666d gui=NONE]]
+-- vim.cmd[[hi PmenuSel ctermfg=NONE ctermbg=24 cterm=NONE guifg=NONE guibg=#204a87 gui=NONE]]
+vim.cmd[[highlight LineNr ctermfg=grey]]
+vim.cmd[[hi Visual cterm=none ctermbg=darkgrey ctermfg=cyan]]
+vim.cmd(":hi Cursorline cterm=NONE ctermbg=236")
+
+-- DEFAULTS
+vim.cmd("set encoding=UTF-8")
 opt.relativenumber = true
 o.clipboard = 'unnamedplus'
 o.number = true
@@ -20,89 +30,34 @@ o.smartcase = true
 o.wrap = false
 o.hidden = true
 o.laststatus = 2
-o.scrolloff = 10
 o.showcmd = true
 o.showmatch = true
-
 o.completeopt = 'menu,menuone,noinsert'
 o.pumheight = 5
 o.cursorline = true
-vim.cmd(":hi Cursorline cterm=NONE ctermbg=236")
 
--- KEYBINDINGS
-vim.cmd [[packadd packer.nvim]]
-local function map(m, k, v)
-    vim.keymap.set(m, k, v, { silent = true })
-end
-
--- KEY-REMAPS
-map('n', '<F5>', '<Esc>:!python %<CR>')
-
-map('i', '<C-d>', '<Bs>')
-map('i', '<C-h>', '<Left>')
-map('i', '<C-j>', '<Down>')
-map('i', '<C-k>', '<Up>')
-map('i', '<C-l>', '<Right>')
-map('c', '<C-h>', '<Left>')
-map('c', '<C-j>', '<Down>')
-map('c', '<C-k>', '<Up>')
-map('c', '<C-l>', '<Right>')
-map('i', '{<CR>', '{<CR>}<Esc><S-O>')
-map('i', '<S-Tab>', '<Esc><<i')
-map('x', '<Tab>', '>')
-map('x', '<S-Tab>', '<')
-map('x', '"', 'c""<Esc>P')
-map('x', "'", "c''<Esc>P")
-map('x', '(', 'c()<Esc>P')
--- map('x', '{', 'c{}<Esc>P')
-map('x', '[', 'c[]<Esc>P')
-map('x', '*', 'c**<Esc>P')
-
-map('i', '<C-b>', '<Esc>:Lexplore<CR>')
-map('n', '<C-b>', '<Esc>:Lexplore<CR>')
+-- KEYBINDINGS / KEY-REMAPS
+vim.keymap.set('n', '<F5>', '<Esc>:!python %<CR>', { silent = true})
+vim.keymap.set('i', '{<CR>', '{<CR>}<Esc><S-O>', { silent = true})
+vim.keymap.set('i', '<S-Tab>', '<Esc><<i', { silent = true})
+vim.keymap.set('x', '<Tab>', '>', { silent = true})
+vim.keymap.set('x', '<S-Tab>', '<', { silent = true})
+vim.keymap.set('x', '"', 'c""<Esc>P', { silent = true})
+vim.keymap.set('x', "'", "c''<Esc>P", { silent = true})
+vim.keymap.set('x', '(', 'c(, { silent = true})<Esc>P', { silent = true})
+vim.keymap.set('x', '[', 'c[]<Esc>P', { silent = true})
+vim.keymap.set('i', '<C-b>', '<Esc>:Lexplore<CR>', { silent = true})
+vim.keymap.set('n', '<C-b>', '<Esc>:Lexplore<CR>', { silent = true})
 
 -- NETRW CONFIG
 g.netrw_banner = 0
-g.netrw_liststyle = 3
+g.netrw_liststyle = 2
 g.netrw_winsize = 30
 g.netrw_altv = 1
 
---[[ -- PLUGIN CONFIGS AND INITS
+--[[ -- ====================================================================
+-- PLUGIN CONFIGS AND INITS
 -- ====================================================================
--- TOOOOKYYYOOO - "ThePrimeagen"
-g.tokynight_transparent_sidebar = true
-g.tokynight_transparent = true
-opt.background = "dark"
-vim.cmd("colorscheme tokyonight")
-
--- Lua line, very nice - "Borat"
-require('lualine').setup {
-  options = {
-    theme = 'gruvbox',
-    component_separators = '|',
-    section_separators = {},
-  },
-  sections = {
-    lualine_a = {{ 'mode', right_padding = 2 },},
-    lualine_b = {'filename', 'branch'},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {'filetype', 'progress'},
-    lualine_z = {{ 'location', left_padding = 2 }},
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
-}
-vim.cmd("set encoding=UTF-8")
-
 -- Block commenting
 require('kommentary.config').use_extended_mappings()
 
@@ -135,15 +90,12 @@ local cmp = require'cmp'
       ['<C-h>'] = cmp.mapping.scroll_docs(-4),
       ['<C-k>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
-      ['<Esc>'] = cmp.mapping.abort(),
+      -- ['<Esc>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+      { name = 'vsnip' },
     }, {
       { name = 'buffer' },
     })
@@ -178,26 +130,13 @@ local cmp = require'cmp'
 
   -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   require('lspconfig')['pyright'].setup {
     capabilities = capabilities
   }
 
 -- DON'T FORGET AFTER INSTALL :: TSInstall html/php/etc... + TSEnable autotag !!
 require('nvim-ts-autotag').setup()
-
--- TELESCOPE FINDER AND IT'S KEY-MAPPING
-require('telescope').setup()
--- nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
--- nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
--- nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
--- nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-
-map('n', '<C-f>f', '<Esc>:Telescope find_files <CR>')
-map('n', '<C-f>g', '<Esc>:Telescope live_grep <CR>')
-map('n', '<C-f>b', '<Esc>:Telescope buffers <CR>')
-map('n', '<C-f>h', '<Esc>:Telescope help_tags <CR>') ]]
--- ====================================================================
+-- ==================================================================== ]]
 
 -- PLUGINS
 return require('packer').startup(function()
@@ -205,26 +144,18 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use 'neovim/nvim-lspconfig'
     use "lukas-reineke/indent-blankline.nvim"
-    use 'folke/tokyonight.nvim'
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
     use 'b3nj5m1n/kommentary'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/nvim-cmp'
+
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+
     use 'windwp/nvim-ts-autotag'
-    use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
 
 end)
