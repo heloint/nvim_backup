f3b593d2cfd525ada73fe89ee18d4295efb92cca
dmajer <majerdaniel93@gmail.com>
2022-12-23 03:14:01 +0100

    try to source the .bashrc a bit later in the script, because it doesn't seem to source it in time

--- new_setup_in_progress.sh
+++ new_setup_in_progress.sh
@@ -17,28 +17,34 @@ $prefix apt install -y gcc \
                        wget \
                        xclip
 
+# Download node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
-source ~/.bashrc
-
-nvm install node
-
-nvim install-latest-npm
-
+# Download neovim latest debian package
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 
+# Install neovim
 $prefix apt install -y ./nvim-linux64.deb
 
+# Create config dir for neovim
 mkdir -p ~/.config/nvim ; cd ~/.config/nvim
 
+# Download and install packer plugin manager.
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git clone https://github.com/heloint/nvim_backup
+# Install node version manager.
+source ~/.bashrc
+nvm install node
+nvim install-latest-npm
 
+# Get my configs.
+git clone https://github.com/heloint/nvim_backup
 cp ./nvim_backup/init.lua .
 
+# Install plugins
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
-# sed -i '56,184s/^-- //g' init.lua
+# Remove comments from the plugin config lines.
+sed -i '56,184s/^-- //g' init.lua
