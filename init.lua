e8d14f8f5e784b5a4cc615bf331f1b43326e75f5
heloint <majerdaniel93@gmail.com>
2023-03-30 14:15:40 +0200

    save undo history after closed session

--- init.lua
+++ init.lua
@@ -7,25 +7,35 @@ local A   = vim.api
 -- ====================================================================
 vim.cmd("set encoding=UTF-8")
 opt.relativenumber = true
+
 o.clipboard = 'unnamedplus'
 o.number = true
 o.mouse= "a"
+
 o.tabstop = 4
 o.shiftwidth = 4
 o.expandtab = true
+
 o.swapfile = false
 o.backup = false
+o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undofile = true
+
 o.smartindent  = true
 o.autoindent = true
+
 o.incsearch = true
 o.ignorecase = true
 o.smartcase = true
+
 o.wrap = false
 o.hidden = true
 o.laststatus = 2
+
 o.showcmd = true
 o.showmatch = true
 o.completeopt = 'menu,menuone,noinsert'
+
 o.pumheight = 5
 o.cursorline = true
 
