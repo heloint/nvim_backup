2ba3eb79e7bc861a621c08b51fe5fef97e7c993f
heloint <majerdaniel93@gmail.com>
2023-04-04 20:31:23 +0200

    revert back to default lsp keymaps

--- lua/heloint/keymaps.lua
+++ lua/heloint/keymaps.lua
@@ -13,17 +13,7 @@ vim.keymap.set('i', '<C-b>', '<Esc>:NvimTreeToggle<CR>', { silent = true})
 vim.keymap.set('n', '<C-b>', '<Esc>:NvimTreeToggle<CR>', { silent = true})
 vim.keymap.set('n', 'H', 'gT', { silent = true})
 vim.keymap.set('n', 'L', 'gt', { silent = true})
-
-
--- Jump to definition shortcut. Using C-Left click can be cumbersome
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
--- Open up a list of references in a quickfix window.
-vim.keymap.set('n', 'gf', '<Esc>:lua vim.lsp.buf.references()<CR>', { silent = true})
--- Show info on hover and ctrl-space.
-vim.keymap.set('n', '<C-space>', '<Esc>:lua vim.lsp.buf.hover()<CR>', { silent = true})
--- Selects a code action available at the current cursor position.
-vim.keymap.set('n', '<Space>q', '<Esc>:lua vim.lsp.buf.code_action()<CR>', { silent = true})
-
+vim.keymap.set('t', '<Esc>', '<C-\\><C-N>', { silent = true})
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>f', builtin.find_files, {})
@@ -35,3 +25,41 @@ vim.keymap.set('n', '<C-f>q', builtin.resume, {})
 -- Block commenting
 -- ====================================================================
 require('kommentary.config').use_extended_mappings()
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    --[[ vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts) ]]
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
