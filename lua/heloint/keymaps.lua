b8c2918cb183cbcad2fe639c5bb60bc6ae172271
heloint <majerdaniel93@gmail.com>
2023-11-08 12:17:18 +0100

    fdsa

--- lua/heloint/keymaps.lua
+++ lua/heloint/keymaps.lua
@@ -88,8 +88,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>d', vim.diagnostic.setqflist, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('x', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set({'v', 'n'}, '<space>ca', vim.lsp.buf.code_action)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
