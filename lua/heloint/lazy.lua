9eaf9f4327f6fa18af6bf4725357043a891b1848
heloint <majerdaniel93@gmail.com>
2024-10-09 12:08:02 +0200

    tbc

--- lua/heloint/lazy.lua
+++ lua/heloint/lazy.lua
@@ -20,35 +20,6 @@ require("lazy").setup({
             "nvim-treesitter/nvim-treesitter",
             build = ":TSUpdate",
         },
-        -- { "rebelot/kanagawa.nvim" },
-        {
-            "rebelot/kanagawa.nvim",
-            lazy = false,
-            config = function()
-                require("kanagawa").setup({
-                    compile = true,
-                    functionStyle = { bold = true },
-                    dimInactive = true,
-                    transparent = true,
-                })
-                vim.cmd("colorscheme kanagawa-dragon")
-            end,
-            override = function(colors)
-                local theme = colors.theme
-                return {
-                    NormalFloat = { bg = "none" },
-                    FloatBorder = { bg = "none" },
-                    FloatTitle = { bg = "none" },
-                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-                    PmenuSbar = { bg = theme.ui.bg_m1 },
-                    PmenuThumb = { bg = theme.ui.bg_p2 },
-                }
-            end,
-        },
         {
             'VonHeikemen/lsp-zero.nvim',
             branch = 'v3.x',
