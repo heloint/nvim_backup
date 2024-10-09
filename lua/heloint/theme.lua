85fb84246e83797b852e503f6f7b12f28013ff13
heloint <majerdaniel93@gmail.com>
2024-10-09 21:07:00 +0200

    tbc

--- lua/heloint/theme.lua
+++ lua/heloint/theme.lua
@@ -1,17 +1,23 @@
 -- COLORS
 -- ====================================================================
-vim.cmd("colorscheme darkplus")
+vim.cmd("colorscheme kanagawa")
 
--- local telescopeColors = {
---     Normal = { bg = "#27272a" },
---     NormalFloat = { bg = "none" },
---     FloatBorder = { bg = "none" },
---     Visual = { bg = "#9ca3af", fg = "#111827", bold = true },
---     TelescopeMatching = { fg = "#b91c1c", bold = true },
---     TelescopeSelection = { fg = "#111827", bg = "#9ca3af" },
---     DiffDelete = { bg = "#b91c1c" },
--- }
---
--- for hl, col in pairs(telescopeColors) do
---     vim.api.nvim_set_hl(0, hl, col)
--- end
+local telescopeColors = {
+    Normal = { bg = "#27272a" },
+    NormalFloat = { bg = "none" },
+    FloatBorder = { bg = "none" },
+
+    TelescopeNormal = { bg = "none" },
+    TelescopeResultsBorder = { bg = "none" },
+    TelescopePromptBorder = { bg = "none" },
+    TelescopePreviewBorder = { bg = "none" },
+
+    DiffDelete = { bg = "#b91c1c" },
+
+    LineNr = { bg = "#212424" },
+    SignColumn = { bg = "#212424" },
+}
+
+for hl, col in pairs(telescopeColors) do
+    vim.api.nvim_set_hl(0, hl, col)
+end
