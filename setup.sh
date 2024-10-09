fbeeccba0dd7e987b3dd1b612699491a084c2988
heloint <majerdaniel93@gmail.com>
2024-10-09 12:10:41 +0200

    tbc

--- setup.sh
+++ setup.sh
@@ -42,8 +42,6 @@ cd ~/.config/nvim && \
 git clone https://github.com/heloint/nvim_backup && \
 cp ./nvim_backup/init.lua . && \
 cp -r ./nvim_backup/lua .
-cp -r ./nvim_backup/queries .
-cp -r ./nvim_backup/after .
 
 # Install Fura Nerdfont (optional, not all environment will have support for icons)
 mkdir -p ~/.local/share/fonts/
