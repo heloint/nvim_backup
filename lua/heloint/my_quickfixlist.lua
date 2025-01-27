4454262fc00e53b68cc7835451b91201f101c429
heloint <majerdaniel93@gmail.com>
2025-01-27 19:08:14 +0100

    tbc

--- lua/heloint/my_quickfixlist.lua
+++ lua/heloint/my_quickfixlist.lua
@@ -1,4 +1,44 @@
-local function Toggle_qf()
+vim.api.nvim_create_autocmd('BufWinEnter', {
+    group = vim.api.nvim_create_augroup('quickfix_list_base_group', { clear = true }),
+    desc = 'allow updating quickfix window',
+    pattern = 'quickfix',
+    callback = function(_)
+        vim.bo.modifiable = true
+        -- :vimgrep's quickfix window display format now includes start and end column (in vim and nvim) so adding 2nd format to match that, also for 'grep -rin'
+        vim.bo.errorformat = '%f|%l col %c| %m,%f|%l col %c-%k| %m,%f|%l| %m'
+        vim.keymap.set(
+            'n',
+            '<C-s>',
+            '<Cmd>cgetbuffer|set nomodified|echo "quickfix/location list updated"<CR>',
+            { buffer = true, desc = 'Update quickfix/location list with changes made in quickfix window' }
+        )
+    end,
+})
+
+-- Autocommand to trigger on grep completion
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+    pattern = "grep", -- Trigger only for grep commands
+    callback = function(_)
+        -- Open the quickfix list
+        vim.cmd("copen")
+        -- If there are matches, open the first result in a horizontal split
+        if not vim.tbl_isempty(vim.fn.getqflist()) then
+            vim.cmd("split") -- Open the result in a horizontal split
+        end
+        vim.bo.modifiable = true
+        -- :vimgrep's quickfix window display format now includes start and end column (in vim and nvim) so adding 2nd format to match that, also for 'grep -rin'
+        vim.bo.errorformat = '%f|%l col %c| %m,%f|%l col %c-%k| %m,%f|%l| %m'
+        vim.keymap.set(
+            'n',
+            '<C-s>',
+            '<Cmd>cgetbuffer|set nomodified|echo "quickfix/location list updated"<CR>',
+            { buffer = true, desc = 'Update quickfix/location list with changes made in quickfix window' }
+        )
+    end,
+})
+
+-- Toggle quickfix list on the bottom of the screen.
+vim.keymap.set('n', '<C-q>', function()
     local qf_exists = false
     for _, win in pairs(vim.fn.getwininfo()) do
         if win["quickfix"] == 1 then
@@ -13,27 +53,7 @@ local function Toggle_qf()
         vim.cmd "copen"
         vim.cmd "set modifiable"
     end
-end
-
-vim.api.nvim_create_autocmd('BufWinEnter', {
-  group = vim.api.nvim_create_augroup('quickfix_list_base_group', { clear = true }),
-  desc = 'allow updating quickfix window',
-  pattern = 'quickfix',
-  callback = function(_)
-    vim.bo.modifiable = true
-    -- :vimgrep's quickfix window display format now includes start and end column (in vim and nvim) so adding 2nd format to match that
-    vim.bo.errorformat = '%f|%l col %c| %m,%f|%l col %c-%k| %m'
-    vim.keymap.set(
-    'n',
-    '<C-s>',
-    '<Cmd>cgetbuffer|set nomodified|echo "quickfix/location list updated"<CR>',
-    { buffer = true, desc = 'Update quickfix/location list with changes made in quickfix window' }
-    )
-  end,
-})
-
--- Toggle quickfix list on the bottom of the screen.
-vim.keymap.set('n', '<C-q>', Toggle_qf, {})
+end, {})
 
 -- Loop through easier the quickfix list, without jumping back n' forth the main screen and the QF list.
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
