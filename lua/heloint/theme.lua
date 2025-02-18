03815cbb03a9bc01ea543944e567198b104a2173
heloint <majerdaniel93@gmail.com>
2025-02-18 15:23:39 +0100

    tbc

--- lua/heloint/theme.lua
+++ lua/heloint/theme.lua
@@ -1,17 +1,11 @@
 -- COLORS
 -- ====================================================================
--- vim.cmd("colorscheme kanagawa")
 vim.cmd("colorscheme retrobox")
 
-local telescopeColors = {
+local customColors = {
     Normal = { bg = "#27272a" },
     NormalFloat = { bg = "none" },
     FloatBorder = { bg = "none" },
-
-    TelescopeNormal = { bg = "none" },
-    TelescopeResultsBorder = { bg = "none" },
-    TelescopePromptBorder = { bg = "none" },
-    TelescopePreviewBorder = { bg = "none" },
 
     DiffDelete = { bg = "#b91c1c" },
 
@@ -19,9 +13,10 @@ local telescopeColors = {
     SignColumn = { bg = "#212424" },
     WinSeparator = { fg = "#f8fafc", bg = "#334155", },
     StatusLine = {bg = "#475569", fg = "#f9fafb"},
+    Visual = { bg = "#314158", ctermfg = nil}
 }
 
-for hl, col in pairs(telescopeColors) do
+for hl, col in pairs(customColors) do
     vim.api.nvim_set_hl(0, hl, col)
 end
 
