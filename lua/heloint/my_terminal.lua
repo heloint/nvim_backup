42ee0b3ff74c89c32135ecfc0e0884317a90c9fc
heloint <majerdaniel93@gmail.com>
2025-03-04 11:44:16 +0100

    tbc

--- lua/heloint/my_terminal.lua
+++ lua/heloint/my_terminal.lua
@@ -1,12 +1,18 @@
-local current_term_bufname = nil
+local terminals_by_tabpages = {}
 
 function Toggle_terminal()
     -- If we don't have a toggleable terminal initialized, then we init one and return.
+    local current_window = vim.api.nvim_get_current_win()
+    local current_tabpage = vim.api.nvim_win_get_tabpage(current_window)
+    local current_term_bufname = terminals_by_tabpages[current_tabpage]
+
     if current_term_bufname == nil or vim.fn.bufexists(current_term_bufname) == 0 then
         vim.cmd.vnew()
         vim.cmd.term()
+
         local current_buffer = vim.api.nvim_get_current_buf()
         current_term_bufname = vim.fn.bufname(current_buffer)
+        terminals_by_tabpages[current_tabpage] = vim.fn.bufname(current_buffer)
         return
     end
 
@@ -26,16 +32,8 @@ function Toggle_terminal()
     vim.cmd('vs | b ' .. current_term_bufname)
 end
 
-vim.api.nvim_create_autocmd({'TermOpen'}, {
-    callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
-    end
-})
-
 vim.keymap.set('n', '<space>t', Toggle_terminal, {})
 
 -- Return to normal mode in the integrated terminal
 -- =========================================================
 vim.keymap.set('t', '<Esc>', '<C-\\><C-N>', { silent = true })
-
