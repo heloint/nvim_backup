41380ae1fcad4245c6f63ec6d4c854f5ca88a293
dmajer <daniel.majer.ext@allianz-trade.com>
2026-01-29 13:45:28 +0100

    tbc

--- lua/heloint/my_grep.lua
+++ lua/heloint/my_grep.lua
@@ -6,9 +6,12 @@ if does_uses_ripgrep and does_unrestricted then
     vim.o.grepprg = without_unrestricted
 end
 
-vim.keymap.set("v", "<space>gt", "y:tabe | silent grep \"<C-r>0\"<left>", { desc = "Grep selected text in new tab." })
-vim.keymap.set("v", "<space>gv", "y:vs | silent grep \"<C-r>0\"<left>",
-    { desc = "Grep selected text in new vertical split." })
-vim.keymap.set("v", "<space>gs", "y:sp | silent grep \"<C-r>0\"<left>",
-    { desc = "Grep selected text in new horizontal split." })
-vim.keymap.set("v", "<space>gg", "y:silent grep \"<C-r>0\"<left>", { desc = "Grep selected text in current window." })
+vim.keymap.set("n", "<space>gt", ":tabe | silent grep \"\"<left>", { desc = "Grep selected text in new tab." })
+vim.keymap.set("n", "<space>gv", ":vs | silent grep \"\"<left>", { desc = "Grep selected text in new vertical split." })
+vim.keymap.set("n", "<space>gs", ":sp | silent grep \"\"<left>", { desc = "Grep selected text in new horizontal split." })
+vim.keymap.set("n", "<space>gg", ":silent grep \"\"<left>", { desc = "Grep selected text in current window." })
+
+vim.keymap.set("v", "<space>gt", "y:tabe | silent grep \"<C-r>0\"", { desc = "Grep selected text in new tab." })
+vim.keymap.set("v", "<space>gv", "y:vs | silent grep \"<C-r>0\"", { desc = "Grep selected text in new vertical split." })
+vim.keymap.set("v", "<space>gs", "y:sp | silent grep \"<C-r>0\"", { desc = "Grep selected text in new horizontal split." })
+vim.keymap.set("v", "<space>gg", "y:silent grep \"<C-r>0\"", { desc = "Grep selected text in current window." })
