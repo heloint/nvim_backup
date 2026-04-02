24af853362f874566d229116fe8033f5654ade7d
dmajer <daniel.majer.ext@allianz-trade.com>
2026-04-02 14:18:06 +0200

    tbc

--- lua/heloint/my_plugins.lua
+++ lua/heloint/my_plugins.lua
@@ -14,20 +14,6 @@ vim.pack.add({
     { src = 'https://github.com/nvim-treesitter/nvim-treesitter-context' },
 
     { src = 'https://github.com/stevearc/conform.nvim' },
-
-    -- -- Specify plugin's name (here the plugin will be called "plugin2"
-    -- -- instead of "generic-name")
-    -- { src = 'https://github.com/user/generic-name',   name = 'plugin2' },
-    --
-    -- -- Specify version to follow during install and update
-    -- {
-    --     src = 'https://github.com/user/plugin3',
-    --     -- Version constraint, see |vim.version.range()|
-    --     version = vim.version.range('1.0'),
-    -- },
-    -- {
-    --     src = 'https://github.com/user/plugin4',
-    --     -- Git branch, tag, or commit hash
-    --     version = 'main',
-    -- },
 })
+
+vim.cmd("packadd nvim.undotree")
